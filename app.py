c83681823f9f2e5cf81c984311e877b7461eb942
otto67 <otto_munthe@yahoo.no>
2021-10-24 19:43:47 +0200

    Added unit testing

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ from matplotlib_test import gravity
 from scipy_test import integrate
 from scipy_test import odesolver
 
+def test():
+    b=0
+    if 'b' in locals():
+        print("Yess")
+    else:
+        print("Nooo")
+
+    if 'b' in globals():
+        print("Yess")
+    else:
+        print("Nooo")
+
 if __name__ == "__main__":
 
+    
+
+    exit(0)
     integrate.integrate(2, 1, 1)
 
     sim = odesolver.FreeFall()
